498712460e23a74e060f57a4727da14efc66bddd
hide-tono <information@tono-n-chi.com>
2017-12-01 09:45:26 +0900

    -

--- pgportfolio/marketdata/datamatrices.py
+++ pgportfolio/marketdata/datamatrices.py
@@ -5,19 +5,42 @@ class DataMatrices:
                  market='poloniex', coin_filter=1, window_size=50, feature_number=3, test_portion=0.15,
                  portion_reserved=False, online=False, is_permed=False):
         """
-        :param start:
-        :param end:
-        :param period:
-        :param batch_size:
+        :param start: 開始時間（UNIX時間）
+        :param end: 終了時間（UNIX時間）
+        :param period:入力行列にアクセスできる期間
+        :param batch_size:バッチサイズ
         :param volume_average_days:
         :param buffer_bias_ratio:
         :param market:
-        :param coin_filter:
-        :param window_size:
-        :param feature_number:
+        :param coin_filter: 使用するコインの数
+        :param window_size:入力データの数
+        :param feature_number:特徴量の数。1～4 (2は未実装)
         :param test_portion:
         :param portion_reserved:
         :param online:
         :param is_permed:
         """
-        pass
+        start = int(start)
+        self.__end = int(end)
+
+        # window_sizeがMIN_NUM_PERIOD以上であること
+        self.__coin_no = coin_filter
+        type_list = get_type_list(feature_number)
+
+
+def get_type_list(feature_number):
+    """
+    特徴量名のリストを返します。
+    :param feature_number: 特徴量の数。1～4 (2は未実装)
+    :return: 1:close, 2:volume, 3:high, low, 4open
+    """
+    if feature_number == 1:
+        return ['close']
+    elif feature_number == 2:
+        raise NotImplementedError("volumeは未実装")
+    elif feature_number == 3:
+        return ['close', 'high', 'low']
+    elif feature_number == 4:
+        return ['close', 'high', 'low', open]
+    else:
+        raise ValueError('feature_numberは1-4で指定してください')
